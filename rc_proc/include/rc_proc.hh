60214f6b31d760f35b9efcf452b1d56a1924dfcf
Salih Tolga Ozaslan <ozaslan@seas.upenn.edu>
2015-06-28 18:36:33 -0400

    -- Major configuration fix in ins_khex_rc_config.yaml. The y-axis was reversed.

--- rc_proc/include/rc_proc.hh
+++ rc_proc/include/rc_proc.hh
@@ -2,6 +2,9 @@
 #define __RC_PROC_HH__
 
 #include <com_msgs/RC.h>
+#include <string>
+
+using namespace std;
 
 class RCProc{
 private:
@@ -18,6 +21,7 @@ private:
 	double _min_psi;
 	double _max_psi;
 	bool   _normalize;
+	bool   _reverse_x, _reverse_y, _reverse_z, _reverse_psi;
 public:
 	inline double & dead_zone_x  (){return _dead_zone_x;}
 	inline double & dead_zone_y  (){return _dead_zone_y;}
@@ -32,7 +36,16 @@ public:
 	inline double & min_psi(){return _min_psi;}
 	inline double & max_psi(){return _max_psi;}
 	inline bool   & normalize(){return _normalize;}
+	inline bool   & reverse_x(){return _reverse_x;}
+	inline bool   & reverse_y(){return _reverse_y;}
+	inline bool   & reverse_z(){return _reverse_z;}
+	inline bool   & reverse_psi(){return _reverse_psi;}
 
+	// This function loads the RC params from the config file at 'config_path'.
+	// If the filte cannot be parsed, it sets the params to their default values.
+	bool load_params(const string &config_path);
+	// This function prints the parameters.
+	bool print_params();
 	// This function gets an RC message, filters the values.
 	bool process(const com_msgs::RC &rc_msg, double &x, double &y, double &z, double &psi);
 	// This function is the same as the other 'process(...)' function except
--- rc_proc/src/rc_proc.cc
+++ rc_proc/src/rc_proc.cc
@@ -1,71 +1,131 @@
 #include "rc_proc.hh"
+#include "yaml-cpp/yaml.h"
 
-bool rc_proc::process(const com_msgs::RC &rc_msg, double &x, double &y, double &z, double &psi){
+bool RCProc::load_params(const string &path){
+	YAML::Node node = YAML::LoadFile(path.c_str());
 
-	double _dead_zone_x;                     
-	double _dead_zone_y;                     
-	double _dead_zone_z;                     
-	double _dead_zone_psi;                   
-	double _min_x;                           
-	double _max_x;                           
-	double _min_y;                           
-	double _max_y;                           
-	double _min_z;                           
-	double _max_z;                           
-	double _min_psi;                         
-	double _max_psi;                         
-	bool   _normalize;        
-
-	int mid_x   = (max_x   + min_x  ) / 2;
-	int mid_y   = (max_y   + min_y  ) / 2;
-	int mid_z   = (max_z   + min_z  ) / 2;
-	int mid_psi = (max_psi + min_psi) / 2;
-
-	double x_stick_val   = rc_msg.right_fb - mid_x;
-	double y_stick_val   = rc_msg.right_rl - mid_y;
-	double z_stick_val   =  rc_msg.left_fb - mid_z;
-	double psi_stick_val =  rc_msg.left_rl - mid_psi;
-
-	// ---
-	if(x_stick_val > dead_zone_x)
-		x_stick_val -= dead_zone_x;
-	else if(x_stick_val < -dead_zone_x)
-		x_stick_val += dead_zone_x;
-	else
-		x_stick_val = 0;
-	// ---
-	if(y_stick_val > dead_zone_y)
-		y_stick_val -= dead_zone_y;
-	else if(y_stick_val < -dead_zone_y)
-		y_stick_val += dead_zone_y;
-	else
-		y_stick_val = 0;
-	// ---
-	if(z_stick_val > dead_zone_z)
-		z_stick_val -= dead_zone_z;
-	else if(z_stick_val < -dead_zone_z)
-		z_stick_val += dead_zone_z;
-	else
-		z_stick_val = 0;
-	// ---
-	if(psi_stick_val > dead_zone_psi)
-		psi_stick_val -= dead_zone_psi;
-	else if(psi_stick_val < -dead_zone_psi)
-		psi_stick_val += dead_zone_psi;
-	else
-		psi_stick_val = 0;
-
-	if(_normalize == true){
-		x_stick_val   /= max_x - mid_x - dead_zone_x;
-		y_stick_val   /= max_y - mid_y - dead_zone_y;
-		z_stick_val   /= max_z - mid_z - dead_zone_z;
-		psi_stick_val /= max_psi - mid_psi - dead_zone_psi;
-	}
+	_dead_zone_x   = node["dead_zone_x"].as<double>();
+	_dead_zone_y   = node["dead_zone_y"].as<double>();
+	_dead_zone_z   = node["dead_zone_z"].as<double>();
+	_dead_zone_psi = node["dead_zone_psi"].as<double>();
+	_min_x = node["min_x"].as<double>();
+	_max_x = node["max_x"].as<double>();
+	_min_y = node["min_y"].as<double>();
+	_max_y = node["max_y"].as<double>();
+  _min_z = node["min_z"].as<double>();
+	_max_z = node["max_z"].as<double>();
+  _min_psi = node["min_psi"].as<double>();
+	_max_psi = node["max_psi"].as<double>();
+  _normalize = node["normalize"].as<bool>();
+  _reverse_x = node["reverse_x"].as<bool>();
+  _reverse_y = node["reverse_y"].as<bool>();
+  _reverse_z = node["reverse_z"].as<bool>();
+  _reverse_psi = node["reverse_psi"].as<bool>();
 
 	return true;
 }
 
-bool rc_proc::process(const com_msgs::RC &rc_msg, vector<double> &proc_vals){
+bool RCProc::print_params(){
+  cout << "dead_zone_[x, y, z, psi] = [" << _dead_zone_x << ", " << _dead_zone_y << ", " << _dead_zone_z << ", " << _dead_zone_psi << "]" << endl;
+  cout << "[min, max]_x     = [" << _min_x   << ", " << _max_x   << "]" << endl;
+  cout << "[min, max]_y     = [" << _min_y   << ", " << _max_y   << "]" << endl;
+  cout << "[min, max]_z     = [" << _min_z   << ", " << _max_z   << "]" << endl;
+  cout << "[min, max]_psi   = [" << _min_psi << ", " << _max_psi << "]" << endl;
+  cout << "[normalize]      = [" << (_normalize ? "TRUE" : "FALSE") << "]" << endl;
+  cout << "reverse_[x, y]   = [" << (_reverse_x ? "TRUE" : "FALSE") << ", " << (_reverse_y ? "TRUE" : "FALSE") << " ]" << endl;
+  cout << "reverse_[z, psi] = [" << (_reverse_z ? "TRUE" : "FALSE") << ", " << (_reverse_psi ? "TRUE" : "FALSE") << " ]" << endl;
+  return true;
+}
+
+bool RCProc::process(const com_msgs::RC &rc_msg, double &x, double &y, double &z, double &psi){
+	
+  int mid_x   = (_max_x   + _min_x  ) / 2;
+	int mid_y   = (_max_y   + _min_y  ) / 2;
+	int mid_z   = (_max_z   + _min_z  ) / 2;
+	int mid_psi = (_max_psi + _min_psi) / 2;
+
+  /*
+  cout << "mid_x   = " << mid_x   << endl;
+  cout << "mid_y   = " << mid_y   << endl;
+  cout << "mid_z   = " << mid_z   << endl;
+  cout << "mid_psi = " << mid_psi << endl;
+
+  cout << "rc_msg = [" << rc_msg.right_fb << ", " << rc_msg.right_rl << ", " << rc_msg.left_fb << ", " << rc_msg.left_rl << "]" << endl;
+  */
+
+	double x_stick_val   = (double)rc_msg.right_fb - mid_x;
+	double y_stick_val   = (double)rc_msg.right_rl - mid_y;
+	double z_stick_val   = (double)rc_msg.left_fb - mid_z;
+	double psi_stick_val = (double)rc_msg.left_rl - mid_psi;
+
+  /*
+  cout << "x_stick_val   = " <<   x_stick_val << endl;
+  cout << "y_stick_val   = " <<   y_stick_val << endl;
+  cout << "z_stick_val   = " <<   z_stick_val << endl;
+  cout << "psi_stick_val = " << psi_stick_val << endl;
+  */
+
+	// ---
+	if(x_stick_val > _dead_zone_x)
+		x_stick_val -= _dead_zone_x;
+	else if(x_stick_val < -_dead_zone_x)
+		x_stick_val += _dead_zone_x;
+	else
+		x_stick_val = 0;
+	// ---
+	if(y_stick_val > _dead_zone_y)
+		y_stick_val -= _dead_zone_y;
+	else if(y_stick_val < -_dead_zone_y)
+		y_stick_val += _dead_zone_y;
+	else
+		y_stick_val = 0;
+	// ---
+	if(z_stick_val > _dead_zone_z)
+		z_stick_val -= _dead_zone_z;
+	else if(z_stick_val < -_dead_zone_z)
+		z_stick_val += _dead_zone_z;
+	else
+		z_stick_val = 0;
+	// ---
+	if(psi_stick_val > _dead_zone_psi)
+		psi_stick_val -= _dead_zone_psi;
+	else if(psi_stick_val < -_dead_zone_psi)
+		psi_stick_val += _dead_zone_psi;
+	else
+		psi_stick_val = 0;
+
+  /*
+  cout << "after filtering :" << endl;
+  cout << "x_stick_val   = " <<   x_stick_val << endl;
+  cout << "y_stick_val   = " <<   y_stick_val << endl;
+  cout << "z_stick_val   = " <<   z_stick_val << endl;
+  cout << "psi_stick_val = " << psi_stick_val << endl;
+  */
+
+	if(_normalize == true){
+		x_stick_val   /= _max_x   - mid_x   - _dead_zone_x;
+		y_stick_val   /= _max_y   - mid_y   - _dead_zone_y;
+		z_stick_val   /= _max_z   - mid_z   - _dead_zone_z;
+		psi_stick_val /= _max_psi - mid_psi - _dead_zone_psi;
+	}
+
+  x   = x_stick_val   * (_reverse_x ? -1 : 1);
+  y   = y_stick_val   * (_reverse_y ? -1 : 1);
+  z   = z_stick_val   * (_reverse_z ? -1 : 1);
+  psi = psi_stick_val * (_reverse_psi ? -1 : 1);
+
+  /*
+  cout << "after normalizing :" << endl;
+  cout << "x_stick_val   = " <<   x_stick_val << endl;
+  cout << "y_stick_val   = " <<   y_stick_val << endl;
+  cout << "z_stick_val   = " <<   z_stick_val << endl;
+  cout << "psi_stick_val = " << psi_stick_val << endl;
+  */
+
+	return true;
+}
+
+bool RCProc::process(const com_msgs::RC &rc_msg, vector<double> &proc_vals){
 	proc_vals.resize(4);
 	return process(rc_msg, proc_vals[0], proc_vals[1], proc_vals[2], proc_vals[3]);
 }
